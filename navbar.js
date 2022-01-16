0aa21d586b0bbd41d5205f00ffb2ee4f008175e4
pseudoSOURABH <87263791+pseudoSOURABH@users.noreply.github.com>
2022-01-16 12:57:12 +0530

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,11 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
+// import { Link } from 'react-router-dom'
+
 
 export default function Navbar(probs) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav
+      className={`navbar navbar-expand-lg navbar-${
+        probs.mode === "light" ? "white" : "white"
+      } bg-${probs.mode === "light" ? "dark" : "dark"}`}
+    >
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+        <a className="navbar-brand" href="#">
           {probs.title}
         </a>
         <button
@@ -22,16 +28,17 @@ export default function Navbar(probs) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
+              <a className="nav-link active" aria-current="page" href="#">
                 Home
               </a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">
+            {/* <li className="nav-item">
+              <a className="nav-link" href="/about">
                 {probs.aboutText}
-              </a>
-            </li>
-            <li className="nav-item dropdown">
+              </Link>
+            </li> */}
+          </ul>
+          {/* <li className="nav-item dropdown">
               <a
                 className="nav-link dropdown-toggle"
                 href="/"
@@ -41,8 +48,8 @@ export default function Navbar(probs) {
                 aria-expanded="false"
               >
                 Contact us
-              </a>
-              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+              </a> */}
+          {/* <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
                 <li>
                   <a className="dropdown-item" href="/">
                     Action
@@ -61,15 +68,15 @@ export default function Navbar(probs) {
                     Something else here
                   </a>
                 </li>
-              </ul>
-            </li>
-            <li className="nav-item">
+              </ul> */}
+          {/* </li> */}
+          {/* <li className="nav-item">
               <a className="nav-link disabled" href="/">
                 Disabled
               </a>
             </li>
-          </ul>
-          <form className="d-flex">
+          </ul> */}
+          {/* <form className="d-flex">
             <input
               className="form-control me-2"
               type="search"
@@ -79,21 +86,43 @@ export default function Navbar(probs) {
             <button className="btn btn-danger" type="submit">
               Search
             </button>
-          </form>
+          </form> */}
+
+          <div
+            className={`form-check form-switch  text-${
+              probs.mode === "dark" ? "white" : "white"
+            }`}
+          >
+            <input
+              className="form-check-input"
+              type="checkbox"
+              role="switch"
+              onClick={probs.toggleMode}
+              id="flexSwitchCheckDefault"
+            />
+            <label
+              className="form-check-label  "
+              htmlFor="flexSwitchCheckDefault "
+            >
+              Mode
+            </label>
+          </div>
         </div>
       </div>
     </nav>
   );
 }
 
-Navbar.propTypes = { title: PropTypes.string
+Navbar.propTypes = {
+  title: PropTypes.string,
   // title: PropTypes.string.isRequired    when we strictly want to pass title in app.js
-  , aboutText: PropTypes.string };
+  aboutText: PropTypes.string,
+};
 
 //we can also set default prop probs
 Navbar.defaultProps = {
-  title:"enter title",
-  aboutText:"enter about"
+  title: "enter title",
+  aboutText: "enter about",
 };
 
 //it works when we dont pas argument there in app.js's component.
