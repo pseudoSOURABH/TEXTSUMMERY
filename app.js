fa0b255ee6770056c461217632eb02a3150cd0ec
pseudoSOURABH <87263791+pseudoSOURABH@users.noreply.github.com>
2022-01-16 12:58:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,83 @@
 import "./App.css";
-
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+// import About from "./components/About";
+import React, { useState } from "react";
+import Alert from "./components/Alert";
+
 // import sourabh from "./components/sourabh";
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// because of deployment
 
 function App() {
+  const toggleMode = () => {
+    if (mode === "dark") {
+      setmode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("light mode has been enabled", "success");
+      document.title = "textutills-lightMode";
+    } else {
+      setmode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("dark mode has been enabled", "success");
+      document.title = "textutills-darkmode";
+      // setInterval(() => {
+      //   document.title="textutiils is amazing"
+      // }, 2000);
+
+      // setInterval(() => {
+      //   document.title="intall it Now"
+      // }, 5000  ,
+      // );many appliactions enable this feature to catch users attention.
+    }
+  };
+
+  const showAlert = (message, type) => {
+    setalert({
+      msg: message,
+      type: type,
+    });
+
+    setTimeout(() => {
+      setalert(null);
+    }, 1000);
+  };
+  const [mode, setmode] = useState("light");
+
+  const [alert, setalert] = useState("");
   return (
     <>
       {/* <sourabh></sourabh> */}
-      <Navbar title="textuttils" aboutText="ABOUT"></Navbar>
-      <div className="container my-3">
-      <TextForm heading="enter the text to analyze"></TextForm>
-      </div>
+      {/* <Router> */}
+        <Navbar
+          title="textuttils"
+          aboutText="ABOUT"
+          mode={mode}
+          toggleMode={toggleMode}
+        ></Navbar>
+
+        <Alert alert={alert}></Alert>
+
+        <div className="container my-3">
+          {/* <Routes> */}
+            {/* <Route exact path="/about" element={<About />}></Route>
+             */}
+             {/* we uncommented it because we have to deploy this app on github and  */}
+             {/* router does'nt work well with github pages. */}
+            {/* <Route
+              exact
+              path="/"
+              element={ */}
+                <TextForm
+                  heading="enter the text to analyze"
+                  showAlert={showAlert}
+                  mode={mode}
+                ></TextForm>
+              {/* }
+            ></Route>
+          </Routes> */}
+        </div>
+      {/* </Router> */}
       {/* <Navbar ></Navbar>    ,it is when we dont pass an argument and it goes away with default argument */}
     </>
   );
